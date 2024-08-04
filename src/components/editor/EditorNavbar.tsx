a8a4897e12c7fcb03fc10954f6561e94cedfe3f0
Aakash-Yadav23 <aakashdev24@gmail.com>
2024-08-04 12:28:44 +0530

    feat: Editor Navbar added

--- src/components/editor/EditorNavbar.tsx
+++ src/components/editor/EditorNavbar.tsx
@@ -1,8 +1,48 @@
-export default  function EditorNavbar( ){
+import { Button } from "../ui/button";
+import { Eye, UserPlus, EyeOff, Play, CloudUpload } from "lucide-react";
 
+export default function EditorNavbar() {
+  return (
+    <div
+      className={
+        "h-20 flex bg-[#111827] justify-between items-center w-full px-10 py-2 border"
+      }
+    >
+      <div className="file-name">
+        <h1>Node.js</h1>
+      </div>
 
-    return <div className={'h-20 w-full px-10 py-2 border'}>
+      <Button
+        variant={"default"}
+        className="bg-green-800 hover:bg-green-700 text-white"
+      >
+        <Play />
+        Run
+      </Button>
 
-        EditorNavbar
+      <div className="flex items-center gap-2">
+        <Button
+          variant={"outline"}
+          className="flex items-center gap-1 relative"
+        >
+          <UserPlus />
+
+          <span className="text-xl">Invite</span>
+        </Button>
+        <Button
+          variant={"outline"}
+          className="flex items-center gap-1 relative"
+        >
+          <Eye />
+          {/* <EyeOff /> */}
+
+          <span className="text-[14px] relative top-1">24</span>
+        </Button>
+        <Button variant={"outline"} className=" flex items-center gap-2">
+          <CloudUpload />
+          Deploy
+        </Button>
+      </div>
     </div>
+  );
 }
